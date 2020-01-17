7059eacf09f158b9c7c50ef45b110cee996018b9
Colin Harfst <colinharfst@gmail.com>
2020-01-17 16:23:16 -0500

    create placeholder components for intended pages

---
+++ src/app/components/judge.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export default function Judge() {
+  return <div>Here's some AAron text</div>;
+}
--- src/app/components/routes.tsx
+++ src/app/components/routes.tsx
@@ -1,5 +1,10 @@
 import * as React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./home";
+import Resume from "./resume";
+import Judge from "./judge";
+import Chess from "./chess";
+import Math from "./math";
 
 export default function Routes() {
   return (
@@ -11,38 +16,38 @@ export default function Routes() {
               <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <Link to="/resume">Resume</Link>
             </li>
             <li>
-              <Link to="/users">Users</Link>
+              <Link to="/judge">Judge</Link>
+            </li>
+            <li>
+              <Link to="/chess">chess</Link>
+            </li>
+            <li>
+              <Link to="/math">Math</Link>
             </li>
           </ul>
         </nav>
 
         <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
           <Route path="/">
             <Home />
+          </Route>
+          <Route path="/resume">
+            <Resume />
+          </Route>
+          <Route path="/judge">
+            <Judge />
+          </Route>
+          <Route path="/chess">
+            <Chess />
+          </Route>
+          <Route path="/math">
+            <Math />
           </Route>
         </Switch>
       </div>
     </Router>
   );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
 }
