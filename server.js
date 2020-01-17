199ea2644edd7336787824b4dcaf64a03bbb0a5a
Colin Harfst <colinharfst@gmail.com>
2020-01-17 16:58:33 -0500

    server.js cleanup

--- server.js
+++ server.js
@@ -2,23 +2,26 @@ const path = require("path");
 const express = require("express");
 const webpack = require("webpack");
 const webpackConfig = require("./webpack.config.js");
+const webpackMidware = require("webpack-dev-middleware");
+const webpackHotMidware = require("webpack-hot-middleware");
+
 const app = express();
 const port = process.env.PORT || 3000;
+const compiler = webpack(webpackConfig);
 
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
 });
 
-let compiler = webpack(webpackConfig);
-
 app.use(
-  require("webpack-dev-middleware")(compiler, {
+  webpackMidware(compiler, {
     noInfo: true,
     publicPath: webpackConfig.output.publicPath,
     stats: { colors: true }
   })
 );
-app.use(require("webpack-hot-middleware")(compiler));
+app.use(webpackHotMidware(compiler));
+
 app.use(express.static(path.resolve(__dirname, "dist")));
 
 app.get("*", (_req, res) => {
